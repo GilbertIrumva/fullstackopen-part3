0e438a5a82c7c26f756ca42ff2cb94f9651d313e
Gilbert Irumva <gilbert@opengates.app>
2026-04-23 14:42:03 +0300

    completed the fourth exercise in part3 {3.4: Phonebook backend step 4 }

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 
-const persons = [
+let persons = [
   { 
     id: "1",
     name: "Arto Hellas", 
@@ -50,6 +50,13 @@ if (person){
     res.status(404).end()
 }
 })
+
+app.delete('/api/persons/:id',(req, res)=>{
+ const id = req.params.id
+ persons = persons.filter(person => person.id  !== id)
+ res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
