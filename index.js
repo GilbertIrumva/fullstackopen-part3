2300bb1328db3546706e50bba14e6e807f646954
Gilbertirumva <gilbertirumva25@gmail.com>
2026-05-05 16:28:05 +0300

    fix: clean duplicate middleware, serve dist, drop watch dep

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
-app.use(express.static('build'))
 
-app.use(express.static('build'))
+const app = express()
+
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
-
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-
-
-morgan.token('body', (req) => JSON.stringify(req.body))
-
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
